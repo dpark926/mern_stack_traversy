104f0f84633f587cc3d9c592a1b80b5c149303bd
Daniel Park <dpeace926@gmail.com>
2019-04-02 12:25:29 -0400

    create ShoppingList, add/remove item

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -8,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar />
-        <h1>Hello World</h1>
+        <ShoppingList />
       </div>
     );
   }
