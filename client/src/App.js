6007e28f3a8466c767ea031e74e91530e8e400f5
Daniel Park <dpeace926@gmail.com>
2019-04-04 17:53:33 -0400

    redux for auth and errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
 import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/authActions";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -16,8 +23,8 @@ class App extends Component {
           <AppNavbar />
           <Container>
             <ItemModal />
+            <ShoppingList />
           </Container>
-          <ShoppingList />
         </div>
       </Provider>
     );
